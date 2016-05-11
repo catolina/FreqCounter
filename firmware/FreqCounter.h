0bcbe73d58bf4103d4c8f8651ccebfe2c37aeef3
catolina <k.m.baranowska93@gmail.com>
2016-05-11 17:37:17 +0200

    Create FreqCounter.h

---
+++ firmware/FreqCounter.h
@@ -0,0 +1,32 @@
+
+#ifndef FreqCounter_h
+#define FreqCounter_h
+
+
+#include <avr/interrupt.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+namespace FreqCounter {
+
+	extern unsigned long f_freq;
+	extern volatile unsigned char f_ready;
+	extern volatile unsigned char f_mlt;
+	extern volatile unsigned int f_tics;
+	extern volatile unsigned int f_period;
+	extern volatile unsigned int f_comp;
+	
+	void start(int ms);
+	
+	
+}
+
+#endif
+
+
+
+
+
